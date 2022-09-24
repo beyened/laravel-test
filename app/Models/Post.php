90e92e9b5525f20a921ebce63c38979531c388fc
Beyene Dabi <beyenedabi5@gmail.com>
2022-09-24 08:43:40 +0300

    Commit Relationship

---
+++ app/Models/Post.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    use SoftDeletes;
+
+    protected $date = ['deleted_at'];
+
+//    protected $table = 'posts';
+//    protected $primaryKey = 'id';
+      protected $fillable = [
+          'title',
+          'body'
+          ];
+
+    // Inverse One-to-One Relationship
+      public function user(){
+
+          return $this->belongsTo('App\Models\User');
+
+      }
+
+}
